15e8a58ef3f36ab80a334d38129b721e827509fa
saurabh <singh.saurab@husky.neu.edu>
2018-10-12 02:05:55 -0400

    first commit

---
+++ src/main/java/com/upgrade/infinitispace/virtualwallet/VirtualwalletApplication.java
@@ -0,0 +1,14 @@
+package com.upgrade.infinitispace.virtualwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualwalletApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(VirtualwalletApplication.class, args);
+
+    }
+}
